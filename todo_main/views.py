bf974aed55d6f0b9036a50e35d29eac682390384
atanuc073 <atanuc073@gmail.com>
2024-12-24 19:01:29 +0530

    designed the template

---
+++ todo_main/views.py
@@ -0,0 +1,8 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request,"home.html")
+    # return HttpResponse("Hello, world. You're at the todo_main index.")
